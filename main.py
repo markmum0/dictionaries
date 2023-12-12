eb27b878ab69e38ea3fa60dac74a74a0cabeaaa2
markmum0 <marckmumo@gmail.com>
2023-12-12 10:27:23 +0300

    second commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# entering and opening the files
+# only 2 files in the dir intro.txt and clown.txt
+name = input('Enter file name: ')
+if len(name) < 1:
+    var = name = 'clown.txt'
+handle = open(name)
+# empty dictionary
+di = dict()
+# cleaning up the values and putting them into a list
+for line in handle:
+    words = line.rstrip()
+    wds = words.split()
+    print(wds)
+    for w in wds:
+        # adding the words in the list into the dictionary both new and existing
+        di[w] = di.get(w,0) + 1
+print(di)
